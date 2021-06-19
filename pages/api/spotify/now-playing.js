9ca890610465120326ec5a3a3c19c94af34d01b0
Vic van Cooten <duveaux1@gmail.com>
2021-06-19 22:05:52 +0200

    Add environment variables to vercel

--- pages/api/spotify/now-playing.js
+++ pages/api/spotify/now-playing.js
@@ -2,7 +2,6 @@ import { getNowPlaying } from "../../../lib/spotify";
 
 export default async (_, res) => {
   const response = await getNowPlaying();
-
   const { is_playing, item } = await response.json();
 
   return res.status(200).json({
